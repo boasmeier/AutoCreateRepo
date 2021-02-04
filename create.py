02735675aa8eeac5bfa960e2819f2fdd35d3c2c5
boasmeier <62314872+boasmeier@users.noreply.github.com>
2021-02-04 14:37:25 +0100

    update: added optional path parameter

--- create.py
+++ create.py
@@ -16,15 +16,24 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--name", required=True,
 	    help="name of the repository to create")
+    ap.add_argument("-p", "--path", required=False,
+	    help="absolute path to create local repo")
     args = vars(ap.parse_args())
 
+    # create session
     g = Github(getConfig('github.ini', 'Auth', 'accesstoken'))
     user = g.get_user()
 
+    # create repo
     repo = user.create_repo(args['name'])
     repo.create_file("README.md", "init commit", "# " + args['name'])
 
-    repoClone = pygit2.clone_repository(repo.git_url, "./" + args['name'])
+    # clone repo and set upstream
+    if args['path'] is None:
+        repoClone = pygit2.clone_repository(repo.git_url, "./" + args['name'])
+    else:
+        repoClone = pygit2.clone_repository(repo.git_url, args['path'] + "/" + args['name'])
+  
     repoClone.remotes.set_url("origin", repo.ssh_url)
     repoClone.remotes.set_push_url("origin", repo.ssh_url)
 
